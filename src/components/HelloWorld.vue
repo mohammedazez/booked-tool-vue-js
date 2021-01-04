7520001b1ed62ac23c6b70498deaeb4613bc5f0e
mohammedazez <muhamadaziz047@gmail.com>
2021-01-04 08:03:31 -0800

    Add scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
+    <p class="pertama">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -37,6 +37,8 @@ export default {
     msg: String
   }
 }
+
+import "../styles/Home.scss";
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
