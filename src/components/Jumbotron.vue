773ed88323e355f38946540458244b3f12ef42c0
mohammedazez <muhamadaziz047@gmail.com>
2021-01-05 05:24:31 -0800

    done jumbotron

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,9 +1,19 @@
 <template>
 <div class="row-jumbotron">
   <div class="column-jumbotron">
-    <img class="gambar-area-one" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609849428/Ice%20Message%20Indonesia/pexels-cottonbro-5076530_q9lisn.jpg"/>
+      <div class="container-text-jumbotron">
+        <img class="gambar-area-one" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609849428/Ice%20Message%20Indonesia/pexels-cottonbro-5076530_q9lisn.jpg"/>
+    </div>
+    
+    <div>
      <img class="gambar-area-two" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609849426/Ice%20Message%20Indonesia/pexels-kevin-ku-577585_vgioja.jpg"/>
+     </div>
   </div>
+
+  <div>
+        <p class="text-jumbotron"><strong>Booked Tool</strong> enables you to <br/> create Interactive Advertisement</p>
+        <button class="button-jumbotron">What is it?</button>
+    </div>
 </div>
 </template>
 
