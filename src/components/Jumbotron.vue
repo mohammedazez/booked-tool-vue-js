dd0b4b3bbe02bf169b68b933fd2f62168919c678
mohammedazez <muhamadaziz047@gmail.com>
2021-01-05 04:54:07 -0800

    jumbotron progress

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,6 +1,10 @@
 <template>
-    <section class="banner-area" id="home">
-</section>
+<div class="row-jumbotron">
+  <div class="column-jumbotron">
+    <img class="gambar-area-one" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609849428/Ice%20Message%20Indonesia/pexels-cottonbro-5076530_q9lisn.jpg"/>
+     <img class="gambar-area-two" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609849426/Ice%20Message%20Indonesia/pexels-kevin-ku-577585_vgioja.jpg"/>
+  </div>
+</div>
 </template>
 
 <script>
