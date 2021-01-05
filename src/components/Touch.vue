357fc1bde0ad25e5100a3a4a6f40077ee859c984
mohammedazez <muhamadaziz047@gmail.com>
2021-01-05 05:55:01 -0800

    touch progress

--- src/components/Touch.vue
+++ src/components/Touch.vue
@@ -1,9 +1,16 @@
 <template>
     <div>
         <section class="touch-area" id="touch">
-            <div class="text-part">
-                <h1>Get in Touch</h1>
-                <p>Want to get in touch? We'd love to her from you. Here's how you can reach us..</p>
+            <div>
+                <img class="touch-img" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609294371/Customer%202/002-insurance_l8ei8i.png"/>
+                <h1 class="text-touch-one">Get in Touch</h1>
+                <p class="text-touch-two">Want to get in touch? We'd love to her from you. Here's how you can reach us..</p>
+            </div>
+            <div>
+                <h1>search</h1>
+            </div>
+            <div>
+                <h1>maps</h1>
             </div>
         </section>
     </div>
