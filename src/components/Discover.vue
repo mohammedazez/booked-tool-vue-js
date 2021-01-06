a38edeed4b65195741817aa50659c6723c5ab9ae
mohammedazez <muhamadaziz047@gmail.com>
2021-01-05 18:44:46 -0800

    navbar done

--- src/components/Discover.vue
+++ src/components/Discover.vue
@@ -1,13 +1,27 @@
 <template>
-<div>
-<section class="discover-area" id="discover">
-  <div class="text-part">
-    <h1>Discover</h1>
-    <p>Booked Tool.</p>
+<div class="container-discover">
+<section class="discover-area" id="discover" data-spy="scroll" data-target=".navbar" data-offset="50">
+  <!-- Teks -->
+  <div class="discover-part">
+    <img class="img-discover" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1608949700/046-ssl_certificate_hxicav.png"/>
+    <h1 class="title-discover-one">DISCOVER</h1>
+    <p class="title-discover-two">BOOKED TOOL</p>
   </div>
+
+
+  
 </section>
 </div>
 </template>
+
+<style scoped>
+  .container-discover {
+    position: relative; 
+  }
+  #section1 {padding-top:50px;height:500px;color: #fff; background-color: #1E88E5;}
+  #section2 {padding-top:50px;height:500px;color: #fff; background-color: #673ab7;}
+  #section3 {padding-top:50px;height:500px;color: #fff; background-color: #ff9800;}
+</style>
 
 <script>
 import "../styles/Discover.scss"
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,33 @@
 <template>
-    <div>
-        <div class="navbooked">
-  <input type="checkbox" id="navbooked-check">
-  <div class="navbooked-header">
-    <div class="navbooked-title">
-      Booked Tool
-    </div>
+<nav class="navbar navbar-default navbar-fixed-top navbooked" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>    
   </div>
+  <div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav navbar-left"> 
+        <li><a href="#home"><img class="logo" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609900545/Ice%20Message%20Indonesia/social-media-marketing_pp1kym.png"/></a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#discover">Discover</a></li>
+      <li><a href="#login">Register</a></li>
+      <li><a href="#touch"><img class="search-navbar" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609900545/Ice%20Message%20Indonesia/loupe_kakugz.png"/></a></li>
+    </ul>
+  </div>
+</nav>
+
+    <!-- <div>
+        <div class="navbooked">
+          <input type="checkbox" id="navbooked-check">
+          <div class="navbooked-header">
+            <div class="navbooked-title"> 
+              <img class="logo" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1608949674/029-transfer_nscany.png"/>
+            </div>
+          </div>
   <div class="navbooked-btn">
     <label for="navbooked-check">
       <span></span>
@@ -16,15 +37,15 @@
   </div>
   
   <div class="navbooked-links">
-    <a href="#home">Home</a>
-    <a href="#discover">Discover</a>
-    <a href="#touch">Touch</a>
-    <a href="#register">Register</a>
-    <a href="#login">Login</a>
-    <a href="#search">Search</a>
+    <a class="nav-teks" href="#home">Home</a>
+    <a class="nav-teks" href="#discover">Discover</a>
+    <a class="nav-teks" href="#login">Register</a>
+    <a class="nav-teks" href="#touch">Search</a>
   </div>
 </div>
-    </div>
+    </div> -->
+
+    
 </template>
 
 <script>
