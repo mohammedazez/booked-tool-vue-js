5da6da93deff6e60b21ac1f775e7f930c53a0292
mohammedazez <muhamadaziz047@gmail.com>
2021-01-05 21:47:38 -0800

    progress discover

--- src/components/Discover.vue
+++ src/components/Discover.vue
@@ -1,43 +1,65 @@
 <template>
-<div>
 <section class="discover-area">
-  <!-- Teks -->
   <div class="discover-part">
     <img class="img-discover" src="https://res.cloudinary.com/dulqd0xea/image/upload/v1609903357/Ice%20Message%20Indonesia/presentation_nezovw.png"/>
     <h1 class="title-discover-one">DISCOVER</h1>
     <p class="title-discover-two">BOOKED TOOL</p>
   </div>
 
-
-  <div>
-    <tabs>
-        <tab name="First tab">
-            First tab content
-        </tab>
-        <tab name="Second tab">
-            Second tab content
-        </tab>
-        <tab name="Third tab">
-            Third tab content
-        </tab>
-    </tabs>
-</div>
-  
+  <div class="container-tab">
+    <Tabs>
+      <Tab name="Discover A" selected="true">
+        <p class="text-tab">
+          | Ciuman di pipi, ciuman di dahi, Apa yang dicium sesudah lah cinta?
+          | Dua tiga kacang tanah, enggak ada pacar yang datang ke rumah.
+          | Dapet kado isinya tomat, Bodo amat!!
+          | Ke pasar, nyari obat gatal, Dasar, gak modal!
+          | Dulu delman, sekarang gokar, dulu teman, sekarang pacar.
+        </p>
+      </Tab>
+      <Tab name="Discover B">
+        <p class="text-tab">
+         | Mata belo, ala komedian.gue sama elo? maunya jadian.
+         | Tunda lapar, makan indomi. hati menggelepar, cintapun bersemi.
+         | Potong kuku, pendek-pendek. hatiku beku, si abang mendadak ngondek.
+         | Beli ketan, beli kain songket. biar udah mantan, kita tetep lengket.
+        </p>
+      </Tab>
+      <Tab name="Discover C">
+        <p class="text-tab">
+          | Makan semangka, makan kedondong. kalau suka, nyatain dong.
+          | Ga punya pendirian, bikin jemu. ga mau sendirian, maunya bobo sama kamu.
+          | Nembak itik, langsung kena. kamu cantik, hey nona!
+          | Kotak amal, digoyang-goyang. kemarin aku diramal, jodohnya sama abang.
+        </p>
+      </Tab>
+      <Tab name="Discover D">
+        <p class="text-tab">
+          | Buah semangka buah duren nggak nyangka gue keren
+          | Jambu merah di dinding jangan marah just kidding
+          | Buah semangka buah manggis nggak nyangka gue manis
+          | Men sana in corpore sano gue maen ke sana, elo maen ke sono!
+        </p>
+      </Tab>
+    </Tabs>
+  </div>
 </section>
-</div>
 </template>
 
 
 <script>
 import "../styles/Discover.scss"
-// import Tabs from 'vue-tabs-component';
+import Tab from '@/components/Tab'
+import Tabs from '@/components/Tabs'
 
-// Vue.use(Tabs);
 
 export default {
-    name: "Discoversection"
-}
-
+    name: 'Discoversection',
+    components: {
+      Tab,
+      Tabs
+    }
+  }
 
 
 
---
+++ src/components/Tabs.vue
@@ -0,0 +1,41 @@
+<template>
+  <article>
+    <header class="tabs">
+      <ul class="tabs-ul">
+        <li  v-for="(tab, index) in tabs" :key="index">
+          <div class="nav-item"
+              :class="{ 'is-active': tab.isActive }"
+              @click="selectTab(tab)">
+            {{ tab.name }}
+          </div>
+        </li>
+      </ul>
+    </header>
+    <section class="tabs-details">
+      <slot></slot>
+    </section>
+  </article>
+</template>
+
+<script>
+import "../styles/Discover.scss"
+
+
+  export default {
+    data: () => {
+      return {
+        tabs: []
+      }
+    },
+    methods: {
+      selectTab(selectedTab) {
+        this.tabs.forEach(tab => {
+          tab.isActive = tab.name === selectedTab.name;
+        });
+      }
+    },
+    created() {
+      this.tabs = this.$children;
+    }
+  }
+</script>
